b1ee0ad2f799f71ab78f5b6c9ba0a54d24767c11
MrTrukiny <carloseduardo.tkn@gmail.com>
2019-07-14 17:29:07 -0500

    Update 'constructorFunction.js'

--- week1-may27-31/constructorFunction.js
+++ week1-may27-31/constructorFunction.js
@@ -12,3 +12,29 @@ const pedro = new Person("Pedro", 72, 1.5);
 
 console.log(pedro.sayHi("María"));
 console.log(pedro.bmi());
+
+function Auto() {
+  this.velocity = 0;
+}
+
+Auto.prototype.accelerate = function(num) {
+  this.velocity += num;
+}
+
+Auto.prototype.brake = function(num) {
+  this.velocity -= num;
+  if (this.velocity < 0) this.velocity = 0;
+}
+
+const a1 = new Auto();
+
+console.log(a1)
+
+console.log(a1.velocity); // 0
+a1.accelerate(1)
+a1.accelerate(2)
+console.log(a1.velocity); // 3
+a1.brake(2);
+console.log(a1.velocity); // 1
+a1.brake(3);
+console.log(a1.velocity); //0
